385943bb9166a9b12fab85368319ded742ba898d
Veniamin Zinevych <mitchvik@gmail.com>
2016-09-07 14:19:48 +0300

    +Sequent and parallel task executors;

--- 1-SequentianCallbacks.js
+++ 1-SequentianCallbacks.js
@@ -1,29 +1,67 @@
 // Sequentian calls and sequentian execution
-
+/*
 readConfig('myConfig', () => {});
 selectFromDb('select * from cities', () => {});
 getHttpPage('http://kpi.ua', () => {});
 readFile('README.md', () => {});
+*/
+
+let fns = [readConfig, selectFromDb, getHttpPage, readFile];
+
+sequentialAsync(fns, () => {console.log('Sequential done!');});
+
+parallelAsync(fns, () => console.log('Parallel done!'));
+
+function parallelAsync(fns, done) {
+  count = 0;
+  fns.forEach((item) => {
+    item('', () => {
+      console.log('Parallel callback');
+      if (++count === fns.length) {
+          done();
+      }
+    })
+  });
+}
+
+function sequentialAsync(fns, done) {
+  fns[0]('', () => {
+    console.log('--> Sequential callback');
+    if (fns.length > 1){
+      sequentialAsync(fns.slice(1), done)
+    } else {
+      done();
+    }
+  });
+}
 
 // Pseudo-Asynchronous Functions
 // having just callbacks but working synchronously
 
 function readConfig(name, callback) {
-  console.log('(1) config loaded');
-  callback({ name });
+  setTimeout(() => {
+    console.log('(1) config loaded');
+    callback({name});
+  }, Math.random()*5000);
 }
 
 function selectFromDb(query, callback) {
-  console.log('(2) SQL query executed');
-  callback([ { name: 'Kiev' } , { name: 'Roma' } ]);
+  setTimeout(() => {
+    console.log('(2) SQL query executed');
+    callback([ { name: 'Kiev' } , { name: 'Roma' } ]);
+  }, Math.random()*5000);
 }
 
 function getHttpPage(url, callback) {
-  console.log('(3) Page retrieved');
-  callback('<html>Some archaic web here</html>');
+  setTimeout(() => {
+    console.log('(3) Page retrieved');
+    callback('<html>Some archaic web here</html>');
+  }, Math.random()*5000);
 }
 
 function readFile(path, callback) {
-  console.log('(4) Readme file loaded');
-  callback('file content');
+  setTimeout(() => {
+    console.log('(4) Readme file loaded');
+    callback('file content');
+  }, Math.random()*5000);
 }
